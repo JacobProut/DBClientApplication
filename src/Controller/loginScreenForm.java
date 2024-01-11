dc3531b7ac7e675bb7196728b7454adfa8908f6b
JacobProut <Jprout@wgu.edu>
2024-01-11 01:56:19 -0600

    Added a ObservableList in "Initialize". -languageComboBoxField.setItems(list); --This allows me to populate the comboBox with English&French Options.
    Added French Language Support:
    Made it so everything besides the "Scene Title" is in French[while French is active in comboBox].

--- src/Controller/loginScreenForm.java
+++ src/Controller/loginScreenForm.java
@@ -1,26 +1,103 @@
 package Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class loginScreenForm {
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.time.ZoneId;
 
-    @FXML private Button LoginButton;
-    @FXML private Label ZoneID;
-    @FXML private ComboBox<?> languageComboBoxField;
-    @FXML private TextField passwordField;
-    @FXML private TextField usernameField;
+public class loginScreenForm implements Initializable {
 
-    @FXML void onActionLogin(ActionEvent event) {
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    private Label ZoneID;
+
+    @FXML
+    private Label labelLanguage;
+
+    @FXML
+    private Label labelPassword;
+
+    @FXML
+    private Label labelTimezone;
+
+    @FXML
+    private Label labelUsername;
+
+    @FXML
+    private ComboBox<String> languageComboBoxField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> list = FXCollections.observableArrayList("English", "French");
+        languageComboBoxField.setItems(list);
+        ZoneID.setText(String.valueOf(ZoneId.systemDefault()));
+
+        try {
+            if (Locale.getDefault().getLanguage().equals("fr")) {
+                ResourceBundle rb = ResourceBundle.getBundle("LanguageBundle/language_fr", Locale.getDefault());
+
+                labelUsername.setText(rb.getString("Username"));
+                labelPassword.setText(rb.getString("Password"));
+                labelTimezone.setText(rb.getString("Timezone"));
+                LoginButton.setText(rb.getString("Login"));
+                labelLanguage.setText(rb.getString("Language"));
+            }
+        } catch (Exception e ) {
+            System.out.println("Error:"); //!!!!!!!!ADD ERRORMSG HERE!!!!!!!!
+        }
+    }
+
+    @FXML
+    void onActionLogin(ActionEvent event){
 
     }
 
-    @FXML void onActionLanguagePicker(ActionEvent event) {
+
+    @FXML
+    void onActionLanguagePicker(ActionEvent event) {
+        languageComboBoxField.getSelectionModel().getSelectedItem();
+
+        try {
+            if (languageComboBoxField.getSelectionModel().getSelectedItem().equals("French")) {
+                ResourceBundle rb = ResourceBundle.getBundle("LanguageBundle/language_fr");
+
+                labelUsername.setText(rb.getString("Username"));
+                labelPassword.setText(rb.getString("Password"));
+                labelTimezone.setText(rb.getString("Timezone"));
+                LoginButton.setText(rb.getString("Login"));
+                labelLanguage.setText(rb.getString("Language"));
+
+            } else if (languageComboBoxField.getSelectionModel().getSelectedItem().equals("English")) {
+                ResourceBundle rb = ResourceBundle.getBundle("LanguageBundle/language_en");
+
+                labelUsername.setText(rb.getString("Username"));
+                labelPassword.setText(rb.getString("Password"));
+                labelTimezone.setText(rb.getString("Timezone"));
+                LoginButton.setText(rb.getString("Login"));
+                labelLanguage.setText(rb.getString("Language"));
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        }
 
     }
-
 }

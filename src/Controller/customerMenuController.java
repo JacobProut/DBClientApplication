ffe910154e94cb0e9fac66e494b57e710d684f48
JacobProut <Jprout@wgu.edu>
2024-01-11 17:52:27 -0600

    Can now access customerMenu GUI Via radiobutton on mainMenuController

--- src/Controller/customerMenuController.java
+++ src/Controller/customerMenuController.java
@@ -2,12 +2,19 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class customerMenuController {
 
@@ -108,7 +115,15 @@ public class customerMenuController {
     }
 
     @FXML
-    void radioButtonViewAllCustomers(ActionEvent event) {
+    void radioButtonViewAllCustomers(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent scene;
+
+        stage = (Stage) ((RadioButton)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/customerMenu.fxml")));
+        stage.setScene(new Scene(scene));
+        stage.show();
+        stage.setTitle("Customer View List");
 
     }
 

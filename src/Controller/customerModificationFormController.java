3664242abb13310d4e14f17f1edbf6db8f87e887
JacobProut <Jprout@wgu.edu>
2024-01-11 20:11:12 -0600

    Added onActionModificationCancel ActionEvent. This cancels the modification and sends the user back to customerMenu.fxml

--- src/Controller/customerModificationFormController.java
+++ src/Controller/customerModificationFormController.java
@@ -2,10 +2,16 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 
 public class customerModificationFormController {
 
@@ -39,8 +45,23 @@ public class customerModificationFormController {
     @FXML
     private Button updateCustomerButton;
 
+    Parent scene;
+    Stage stage;
+
     @FXML
-    void onActionModificationCancel(ActionEvent event) {
+    void onActionModificationCancel(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close Modification Page");
+        alert.setHeaderText("Are you sure you want to leave without saving?");
+        alert.setContentText("Click 'OK' to confirm deletion.\r" + "Click 'Cancel' to go back.");
+        Optional<ButtonType> confirmation = alert.showAndWait();
+        if (confirmation.isPresent() && confirmation.get() == ButtonType.OK) {
+            stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/customerMenu.fxml")));
+            stage.setScene(new Scene(scene));
+            stage.show();
+            stage.setTitle("Customer View List");
+        }
 
     }
 

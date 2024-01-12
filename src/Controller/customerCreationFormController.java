4db3bf51a33e122d00a673b41df460a41d4e51f7
JacobProut <Jprout@wgu.edu>
2024-01-11 19:38:45 -0600

    onActionCreationCancel ActionEvent working. Takes user back to customerMenu.fxml

--- src/Controller/customerCreationFormController.java
+++ src/Controller/customerCreationFormController.java
@@ -2,10 +2,17 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.naming.PartialResultException;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 
 public class customerCreationFormController {
 
@@ -39,13 +46,28 @@ public class customerCreationFormController {
     @FXML
     private Button saveButton;
 
+    Parent scene;
+    Stage stage;
+
     @FXML
     void onActionCountryPicker(ActionEvent event) {
 
     }
 
     @FXML
-    void onActionCreationCancel(ActionEvent event) {
+    void onActionCreationCancel(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close Creation Page");
+        alert.setHeaderText("Are you sure you want to leave without saving?");
+        alert.setContentText("Click 'OK' to confirm deletion.\r" + "Click 'Cancel' to go back.");
+        Optional<ButtonType> confirmation = alert.showAndWait();
+        if (confirmation.isPresent() && confirmation.get() == ButtonType.OK) {
+            stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/customerMenu.fxml")));
+            stage.setScene(new Scene(scene));
+            stage.show();
+            stage.setTitle("Customer View List");
+        }
 
     }
 

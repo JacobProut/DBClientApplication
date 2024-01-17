9585d33cb14bf333ff3fc3e4cd5d77b2670e8489
JacobProut <Jprout@wgu.edu>
2024-01-17 13:33:30 -0600

    CountriesDAO.java Created - getAllCountriesData() Method added [Not sure if its even needed] ~ More will need to be added in here in the future

---
+++ src/DAO/CountriesDAO.java
@@ -0,0 +1,42 @@
+package DAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Countries;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class CountriesDAO {
+
+    public static ObservableList<Countries> getAllCountriesData() {
+        ObservableList<Countries> countriesObservableList = FXCollections.observableArrayList();
+
+        try {
+            String sql = "SELECT * FROM countries";
+            PreparedStatement preparedStatement = JDBC.connection.prepareStatement(sql);
+            ResultSet result = preparedStatement.executeQuery();
+
+            while (result.next()) {
+                int countryId = result.getInt("Country_ID");
+                String country = result.getString("Country");
+                LocalDateTime createDate = result.getTimestamp("Create_Date").toLocalDateTime();
+                String createdBy = result.getString("Created_By");
+                LocalDateTime lastUpdate = result.getTimestamp("Last_Update").toLocalDateTime();
+                String lastUpdatedBy = result.getString("Last_Updated_By");
+
+
+                Countries allCountriesData = new Countries(countryId, country, createDate, createdBy, lastUpdate, lastUpdatedBy);
+                countriesObservableList.add(allCountriesData);
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return countriesObservableList;
+    }
+
+
+}

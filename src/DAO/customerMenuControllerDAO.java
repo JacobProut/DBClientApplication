7aa9dd7a44971bdffe2a3ba255b0202843c14409
JacobProut <Jprout@wgu.edu>
2024-01-16 20:14:32 -0600

    Created customerMenuControllerDAO.java - Not sure if its complete or not

---
+++ src/DAO/customerMenuControllerDAO.java
@@ -0,0 +1,48 @@
+package DAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Contacts;
+import model.Customers;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+
+public class customerMenuControllerDAO {
+
+    public static ObservableList<Customers> getAllCustomers() {
+        ObservableList<Customers> customersObservableList = FXCollections.observableArrayList();
+        try {
+            String sql = "SELECT * FROM contacts";
+            PreparedStatement preparedStatement = JDBC.connection.prepareStatement(sql);
+            ResultSet result = preparedStatement.executeQuery();
+
+            while (result.next()) {
+                int customerId = result.getInt("Customer_ID");
+                String customerName = result.getString("Customer_Name");
+                String customerAddress = result.getString("Address");
+                String customerPostalCode = result.getString("Postal_Code");
+                String customerPhoneNumber = result.getString("Phone");
+                LocalDateTime createDate = result.getTimestamp("Create_Date").toLocalDateTime();
+                String createdBy = result.getString("Created_By");
+                LocalDateTime lastUpdated = result.getTimestamp("Last_Update").toLocalDateTime();
+                String lastUpdatedBy = result.getString("Last_Updated_By");
+                int divisionId = result.getInt("Division_ID");
+
+
+                Customers allCustomers = new Customers(customerId, customerName, customerAddress, customerPostalCode, customerPhoneNumber, createDate, createdBy, lastUpdated, lastUpdatedBy, divisionId);
+                customersObservableList.add(allCustomers);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return customersObservableList;
+    }
+
+
+
+
+
+}

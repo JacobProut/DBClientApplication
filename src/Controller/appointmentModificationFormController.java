58b0ae42c9bd147276f4b4a8c6e773bdfc37d817
JacobProut <Jprout@wgu.edu>
2024-01-17 22:41:54 -0600

    added onActionCancelAppointmentModification - Send user back to Appointment Scheduler Form.

--- src/Controller/appointmentModificationFormController.java
+++ src/Controller/appointmentModificationFormController.java
@@ -2,12 +2,19 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 
 public class appointmentModificationFormController {
+    Stage stage;
+    Parent scene;
 
     @FXML
     private TextField appointmentModificationAppointmentID;
@@ -52,8 +59,20 @@ public class appointmentModificationFormController {
     private Spinner<?> startTimeMinuteSpinner;
 
     @FXML
-    void onActionCancelAppointmentModification(ActionEvent event) {
+    void onActionCancelAppointmentModification(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close Appointment Modification Page");
+        alert.setHeaderText("Are you sure you want to leave without saving?");
+        alert.setContentText("Click 'OK' to go back to Appointment Scheduler.\r" + "Click 'Cancel' to stay on the Appointment Modification Form");
+        Optional<ButtonType> confirmation = alert.showAndWait();
+        if (confirmation.isPresent() && confirmation.get() == ButtonType.OK) {
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/mainMenu.fxml")));
+            stage.setScene(new Scene(scene));
+            stage.show();
+            stage.setTitle("Appointment Scheduler");
 
+        }
     }
 
     @FXML

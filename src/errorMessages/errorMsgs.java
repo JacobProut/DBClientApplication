52e548eb979aa159ad7f88f6c86193a470410c2e
JacobProut <Jprout@wgu.edu>
2024-01-18 16:58:13 -0600

    Updated ErrorCodes

--- src/errorMessages/errorMsgs.java
+++ src/errorMessages/errorMsgs.java
@@ -6,29 +6,37 @@ import javafx.scene.control.Alert;
 import java.util.ResourceBundle;
 
 public abstract class errorMsgs implements Initializable {
-    static ResourceBundle languageBundle = ResourceBundle.getBundle("language/lang");
+    static ResourceBundle languageBundle = ResourceBundle.getBundle("LanguageBundle/language");
 
     public static void errorCodes(int codes) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
             switch (codes) {
-                case 1:
-                    alert.setTitle(languageBundle.getString("BadUsername"));
-                    alert.setContentText(languageBundle.getString("ErrorUsername"));
+                case 1: //Empty username field
+                    alert.setTitle(languageBundle.getString("EmptyUsernameField"));
+                    alert.setContentText(languageBundle.getString("EmptyUserName"));
                     alert.showAndWait();
                     break;
 
-                case 2:
-                    alert.setTitle(languageBundle.getString("BadPassword"));
-                    alert.setContentText((languageBundle.getString("ErrorPassword")));
+                case 2: //Empty password field
+                    alert.setTitle(languageBundle.getString("EmptyPasswordField"));
+                    alert.setContentText((languageBundle.getString("EmptyPassword")));
                     alert.showAndWait();
                     break;
 
-                case 3:
-                    alert.setTitle((languageBundle.getString("BadUserOrPass")));
-                    alert.setContentText(languageBundle.getString("ErrorUserOrPass"));
+                case 3: //Incorrect username
+                    alert.setTitle((languageBundle.getString("IncorrectUsernameAndPassword")));
+                    alert.setContentText(languageBundle.getString("wrongUserNameAndPassword"));
                     alert.showAndWait();
                     break;
+
+                case 4:
+                    alert.setTitle((languageBundle.getString("EmptyUserAndPassword")));
+                    alert.setContentText(languageBundle.getString("EmptyUsernPass"));
+                    alert.showAndWait();
+                    break;
+
+
             }
         }
 
-    }
+}

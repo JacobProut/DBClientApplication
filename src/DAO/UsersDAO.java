fd69b6b446ebc1e03e3d250dea8ec717d8478d02
JacobProut <Jprout@wgu.edu>
2024-01-18 17:01:48 -0600

    added getAllUsers() method - No clue when its going to be used yet.
    Added verifyLoginInformation that is used in onActionLogin for loginScreenForm.java

--- src/DAO/UsersDAO.java
+++ src/DAO/UsersDAO.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.Users;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,9 +12,8 @@ import java.time.LocalDateTime;
 
 public class UsersDAO {
 
-
-    //NO IDEA IF THIS IS EVEN RIGHT!
-    public static ObservableList<Users> getAllUsersData() {
+    //Not sure if ill need this in the future or not DONT DELETE UNTIL FORSURE
+    /*public static ObservableList<Users> getAllUsers() {
         ObservableList<Users> usersObservableList = FXCollections.observableArrayList();
 
         try {
@@ -30,7 +30,7 @@ public class UsersDAO {
                 LocalDateTime lastUpdate = result.getTimestamp("Last_Update").toLocalDateTime();
                 String lastUpdatedBy = result.getString("Last_Updated_By");
 
-                Users allUsers = new Users(userId, userName, password, createDate,createBy, lastUpdate, lastUpdatedBy);
+                Users allUsers = new Users(userId, userName, password, createDate, createBy, lastUpdate, lastUpdatedBy);
                 usersObservableList.add(allUsers);
             }
         }
@@ -38,8 +38,38 @@ public class UsersDAO {
             throw new RuntimeException(e);
         }
         return usersObservableList;
+    }*/
+
+    public static ObservableList<Users> getAllUsers() {
+        Connection connection;
+        ObservableList<Users> usersObservableList = FXCollections.observableArrayList();
+        try {
+            connection = JDBC.getConnection();
+            String sql = "SELECT User_ID, User_Name FROM users;";
+            ResultSet result = connection.createStatement().executeQuery(sql);
+            while(result.next()) {
+                Users allUsers = new Users(result.getInt("User_ID"), result.getString("User_Name"));
+                usersObservableList.add(allUsers);
+            }
+            return usersObservableList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-
+    public static boolean verifyLoginInformation(String username, String password) throws SQLException {
+        String checkingLogin = "SELECT * FROM users WHERE User_Name=? AND Password=?";
+        PreparedStatement preparedStatement = JDBC.getConnection().prepareStatement(checkingLogin);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+        try {
+            preparedStatement.execute();
+            ResultSet result = preparedStatement.getResultSet();
+            return (result.next());
+        } catch (Exception e) {
+            System.out.println("ERROR OCCURRED: " + e.getMessage());
+            return false;
+        }
+    }
 
 }

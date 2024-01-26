d1ca561432c63b4570c066d23f6dc374ba34acc8
JacobProut <Jprout@wgu.edu>
2024-01-25 19:35:48 -0600

    Updated errorMsgs & languages.properties

--- src/errorMessages/errorMsgs.java
+++ src/errorMessages/errorMsgs.java
@@ -35,6 +35,47 @@ public abstract class errorMsgs implements Initializable {
                     alert.showAndWait();
                     break;
 
+                case 5: //Add/Modify Customer menu: ALL FIELDS EMPTY
+                    alert.setTitle((languageBundle.getString("addModifyCustomerAllEmptyFields")));
+                    alert.setContentText(languageBundle.getString("AllEmptyAddModifyCustomers"));
+                    alert.showAndWait();
+                    break;
+
+                case 6: //Add/Modify Customer menu: Empty Customer Name
+                    alert.setTitle((languageBundle.getString("EmptyCustomerNameField")));
+                    alert.setContentText(languageBundle.getString("EmptyCustomerName"));
+                    alert.showAndWait();
+                    break;
+
+                case 7: //Add/Modify Customer menu: Empty Address
+                    alert.setTitle((languageBundle.getString("EmptyCustomerAddressField")));
+                    alert.setContentText(languageBundle.getString("EmptyCustomerAddress"));
+                    alert.showAndWait();
+                    break;
+
+                case 8: //Add/Modify Customer menu: Empty Postal Code
+                    alert.setTitle((languageBundle.getString("EmptyCustomerPostalCodeField")));
+                    alert.setContentText(languageBundle.getString("EmptyCustomerPostalCode"));
+                    alert.showAndWait();
+                    break;
+
+                case 9: //Add/Modify Customer menu: Empty Phone Number
+                    alert.setTitle((languageBundle.getString("EmptyCustomerPhoneNumberField")));
+                    alert.setContentText(languageBundle.getString("EmptyCustomerPhoneNumber"));
+                    alert.showAndWait();
+                    break;
+
+                case 10: //Add/Modify Customer menu: Empty Country Box
+                    alert.setTitle((languageBundle.getString("EmptyCustomerCountryBoxField")));
+                    alert.setContentText(languageBundle.getString("EmptyCustomerCountryBox"));
+                    alert.showAndWait();
+                    break;
+
+                case 11: //Add/Modify Customer menu: Empty Division Box
+                    alert.setTitle((languageBundle.getString("EmptyCustomerDivisionsBoxField")));
+                    alert.setContentText(languageBundle.getString("EmptyCustomerDivisionsBox"));
+                    alert.showAndWait();
+                    break;
 
             }
         }

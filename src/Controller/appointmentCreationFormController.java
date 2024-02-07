cf41725e9fdded30a85e3fa7da834bdd13a8802f
JacobProut <Jprout@wgu.edu>
2024-02-06 21:56:46 -0600

    added appointFieldsEmpty() Method + errorcodes added too

--- src/Controller/appointmentCreationFormController.java
+++ src/Controller/appointmentCreationFormController.java
@@ -1,6 +1,9 @@
 package Controller;
 
+import DAO.AppointmentsDAO;
 import DAO.ContactsDAO;
+import DAO.CustomersDAO;
+import DAO.UsersDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,9 +16,9 @@ import model.Contacts;
 import model.Customers;
 import model.Users;
 
-import javax.naming.PartialResultException;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Objects;
@@ -73,10 +76,25 @@ public class appointmentCreationFormController implements Initializable {
 
     @FXML
     void onActionCreateAppointment(ActionEvent event) {
-        String title = appointmentCreationTitle.getText();
-        String description = appointmentCreationDescription.getText();
-        String location = appointmentCreationLocation.getText();
+        try {
+            if (appointFieldsEmpty()) {
+                String title = appointmentCreationTitle.getText();
+                String description = appointmentCreationDescription.getText();
+                String location = appointmentCreationLocation.getText();
+                String type = appointmentCreationType.getText();
+                LocalDateTime startOfAppointment = LocalDateTime.of(startDateCalendar.getValue(), comboBoxStartTime.getValue());
+                LocalDateTime endOfAppointment = LocalDateTime.of(endDateCalendar.getValue(), comboBoxEndTime.getValue());
+                int customerId = customerComboBox.getValue().getCustomerId();
+                int userId = userComboBox.getValue().getUserId();
+                int contactId = contactComboBox.getValue().getContactId();
 
+                //AppointmentsDAO.createAppointments(title, description, location, type, startOfAppointment, endOfAppointment, customerId, userId, contactId);
+                //mainMenuController.returnToAppointments(event);
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
@@ -118,32 +136,60 @@ public class appointmentCreationFormController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         contactComboBox.setItems(ContactsDAO.getAllContacts());
+        userComboBox.setItems(UsersDAO.getAllUsers());
+        customerComboBox.setItems(CustomersDAO.getAllCustomers());
     }
 
 
 
     //add appointment empty fields. MAY REMAKE SO IT INCLUDES errorMessages.errorMsg.errorCode();
-    /*public boolean appointFieldsEmpty() {
+    public boolean appointFieldsEmpty() {
         if (appointmentCreationTitle.getText().isBlank() || appointmentCreationTitle.getText().isEmpty()) {
-            return true;
+            errorMessages.errorMsgs.errorCodes(16);
+            return false;
         }
         else if (appointmentCreationDescription.getText().isBlank() || appointmentCreationDescription.getText().isEmpty()) {
-            return true;
+            errorMessages.errorMsgs.errorCodes(17);
+            return false;
         }
         else if (appointmentCreationLocation.getText().isBlank() || appointmentCreationLocation.getText().isEmpty()) {
-            return true;
-        }
-        else if (appointmentCreationContact.getText().isBlank() || appointmentCreationContact.getText().isEmpty()) {
-            return true;
+            errorMessages.errorMsgs.errorCodes(18);
+            return false;
         }
         else if (appointmentCreationType.getText().isBlank() || appointmentCreationType.getText().isEmpty()) {
-            return true;
+            errorMessages.errorMsgs.errorCodes(19);
+            return false;
         }
-        else if (startDatefxid.getValue() == null) {
-            return true;
+        else if (startDateCalendar.getValue() == null) {
+            errorMessages.errorMsgs.errorCodes(20);
+            return false;
         }
 
-        return false;
+        //ADD ERROR FOR START TIME COMBOBOX BEING NULL
+        // errorCode(21)
+
+        else if (endDateCalendar.getValue() == null) {
+            errorMessages.errorMsgs.errorCodes(22);
+            return false;
+        }
+
+        //ADD ERROR FOR END TIME COMBOBOX BEING NULL
+        // errorCode(23)
+
+        else if (customerComboBox.getValue() == null) {
+            errorMessages.errorMsgs.errorCodes(24);
+            return false;
+        }
+        else if (userComboBox.getValue() == null) {
+            errorMessages.errorMsgs.errorCodes(25);
+            return false;
+        }
+        else if (contactComboBox.getValue() == null) {
+            errorMessages.errorMsgs.errorCodes(26);
+            return false;
+        }
+
+        return true;
     }
-*/
+
 }

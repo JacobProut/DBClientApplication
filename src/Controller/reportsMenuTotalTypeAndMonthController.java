279dfea8c13da73ab4a6e2d313316a09b980b48a
JacobProut <Jprout@wgu.edu>
2024-02-16 14:54:19 -0600

    Initialized appointment columns for type / count

--- src/Controller/reportsMenuTotalTypeAndMonthController.java
+++ src/Controller/reportsMenuTotalTypeAndMonthController.java
@@ -1,44 +1,62 @@
 package Controller;
 
+import DAO.AppointmentsDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Appointments;
+import java.sql.SQLException;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class reportsMenuTotalTypeAndMonthController {
+import static DAO.AppointmentsDAO.getAppointmentTypeTotal;
+
+public class reportsMenuTotalTypeAndMonthController implements Initializable {
     Parent scene;
     Stage stage;
 
 
     @FXML
-    private TableColumn<?, ?> appointmentColMonth;
+    private TableColumn<Appointments, String> appointmentColMonth;
 
     @FXML
-    private TableColumn<?, ?> appointmentColMonthTotalAmount;
+    private TableColumn<Appointments, Integer> appointmentColMonthTotalAmount;
 
     @FXML
-    private TableColumn<?, ?> appointmentColType;
+    private TableColumn<Appointments, String> appointmentColType;
 
     @FXML
-    private TableColumn<?, ?> appointmentColTypeTotalAmount;
+    private TableColumn<Appointments, Integer> appointmentColTypeTotalAmount;
 
     @FXML
-    private TableView<?> tableViewAppointmentMonth;
+    private TableView<Appointments> tableViewAppointmentMonth;
 
     @FXML
-    private TableView<?> tableViewAppointmentType;
+    private TableView<Appointments> tableViewAppointmentType;
 
     @FXML
     void onActionReturnToReportsMenu(ActionEvent event) throws IOException {
         reportsMenuController.returnToReportsMenu(event);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tableViewAppointmentType.setPlaceholder(new Label("NONTHING"));
+        try {
+            tableViewAppointmentType.setItems(getAppointmentTypeTotal());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        appointmentColType.setCellValueFactory(new PropertyValueFactory<>("appointmentType"));
+        appointmentColTypeTotalAmount.setCellValueFactory(new PropertyValueFactory<>("typeCountTotal"));
+
+    }
 }

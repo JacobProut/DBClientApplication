522b28c57e85133369e798b357eb458743159f47
JacobProut <Jprout@wgu.edu>
2024-02-20 14:31:18 -0600

    Added loadAppointmentMenu(ActionEvent). Made login code cleaner + Added 15minute from login appointment check

--- src/Controller/loginScreenForm.java
+++ src/Controller/loginScreenForm.java
@@ -1,6 +1,8 @@
 package Controller;
 
+import DAO.AppointmentsDAO;
 import DAO.UsersDAO;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +10,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Appointments;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -19,6 +23,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -26,6 +31,7 @@ import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
+import static javafx.scene.control.Alert.AlertType.INFORMATION;
 import static utility.errorMessages.errorCode;
 
 public class loginScreenForm implements Initializable {
@@ -58,7 +64,7 @@ public class loginScreenForm implements Initializable {
     }
 
     public boolean isLoginTrue = false;
-    public void onActionLogin(ActionEvent actionEvent) throws SQLException, IOException {
+    /*public void onActionLogin(ActionEvent actionEvent) throws SQLException, IOException {
         if (!loginInfoValidation()) return;
         boolean isLoginValid = UsersDAO.verifyLoginInformation(usernameField.getText(), passwordField.getText());
 
@@ -74,6 +80,46 @@ public class loginScreenForm implements Initializable {
             System.out.println("Successfully Logged in: Loading Appointment Scheduler.");
             isLoginTrue = true;
             timesAttemptedToLogin();
+        }
+    }*/
+
+    public void onActionLogin(ActionEvent actionEvent) throws SQLException, IOException {
+        if (!loginInfoValidation()) return;
+        boolean isLoginValid = UsersDAO.verifyLoginInformation(usernameField.getText(), passwordField.getText());
+
+        if (isLoginValid) {
+            UsersDAO.verifyLoginInformation(usernameField.getText(), passwordField.getText());
+            int loggedInUserID = UsersDAO.getUsersNameById(usernameField.getText());
+            ObservableList<Appointments> appointmentsUserList = AppointmentsDAO.getAppointmentForUserList(loggedInUserID);
+            loadAppointmentMenu(actionEvent);
+            isLoginTrue = true;
+            timesAttemptedToLogin();
+
+            boolean hasAppointmentsOnLogin = false;
+            for (Appointments selectedAppointment : appointmentsUserList) {
+                LocalDateTime now = LocalDateTime.now();
+                LocalDateTime start = selectedAppointment.getStartTime();
+                LocalDate startDate = selectedAppointment.getStartTime().toLocalDate();
+                LocalDateTime nowPlus15Minutes = LocalDateTime.now().plusMinutes(15);
+                DateTimeFormatter selectedTimeFormat = DateTimeFormatter.ofPattern("hh:mm:ss a");
+
+                if ((start.isAfter(now) || start.isEqual(nowPlus15Minutes)) && (start.isBefore(nowPlus15Minutes) || start.isEqual(now))) {
+                    Alert checkIfAppointmentsWithin15MinsOfLogin = new Alert(INFORMATION);
+                    checkIfAppointmentsWithin15MinsOfLogin.setTitle("You have an appointment");
+                    checkIfAppointmentsWithin15MinsOfLogin.setHeaderText("There is an appointment within the next 15 minutes!\n" + "Todays date is: [" + startDate + "]");
+                    checkIfAppointmentsWithin15MinsOfLogin.setContentText("Selected Appointment_ID of [" + selectedAppointment.getAppointmentId() + "] starts at " + selectedTimeFormat.format(selectedAppointment.getStartTime()) + ".");
+                    checkIfAppointmentsWithin15MinsOfLogin.showAndWait();
+                    hasAppointmentsOnLogin = true;
+                }
+                else if (!hasAppointmentsOnLogin) {
+                    Alert noAppointmentsWithInNext15Minutes = new Alert(INFORMATION);
+                    noAppointmentsWithInNext15Minutes.setTitle("You have NO Appointments scheduled");
+                    noAppointmentsWithInNext15Minutes.setHeaderText("No Appointments Scheduled for at least the next 15 minutes!");
+                    noAppointmentsWithInNext15Minutes.setContentText("There are NO Appointments scheduled within the next 15 minutes!");
+                    noAppointmentsWithInNext15Minutes.showAndWait();
+                    break;
+                }
+            }
         }
     }
 
@@ -122,6 +168,17 @@ public class loginScreenForm implements Initializable {
             loginWriter.write("[" + currentDate + "]" + " User: '" + usernameField.getText() + "' has been Denied access to the database at a time of: [" + selectedTimeFormat.format(currentTime) + "]" + "\n");
         }
         loginWriter.close();
+    }
+
+    public static void loadAppointmentMenu(ActionEvent event) throws IOException {
+        Parent login = FXMLLoader.load(Objects.requireNonNull(loginScreenForm.class.getResource("/View/mainMenu.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene mainMenu = new Scene(login);
+        stage.setTitle("Appointment Scheduler Form");
+        stage.setScene(mainMenu);
+        stage.show();
+        stage.centerOnScreen();
+        System.out.println("Successfully Logged in: Loading Appointment Scheduler.");
     }
 
 }

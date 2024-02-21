ad187cc8f4e485740b81d889863b6a332a0291ce
JacobProut <Jprout@wgu.edu>
2024-02-20 20:55:00 -0600

    Finished JavaDoc Comments

--- src/model/Contacts.java
+++ src/model/Contacts.java
@@ -1,49 +1,61 @@
 package model;
 
+
+/**
+ * Contacts class is created for recording objects into tables/ComboBoxes
+ */
 public class Contacts {
 
-    //Primary Key
+
+    /**
+     * Primary Key Declarations
+     */
     private int contactId;
-
-
     private String contactName;
     private String contactEmail;
 
+    /**
+     * Constructor used in ContactsDAO.getAllContacts()
+     * @param contactId
+     * @param contactName
+     * @param contactEmail
+     */
     public Contacts(int contactId, String contactName, String contactEmail) {
         this.contactId = contactId;
         this.contactName = contactName;
         this.contactEmail = contactEmail;
     }
 
+    /**
+     * Constructor used in ContactsDAO.getAllContactsById(int contactId)
+     * @param collectedId
+     * @param collectedName
+     */
     public Contacts(int collectedId, String collectedName) {
         this.contactId = collectedId;
         this.contactName = collectedName;
     }
 
+    /**
+     * getContactId gets the contactId
+     * @return contactId
+     */
     public int getContactId() {
         return contactId;
     }
 
+    /**
+     * setContactId sets the contactId
+     * @param contactId
+     */
     public void setContactId(int contactId) {
         this.contactId = contactId;
     }
 
-    public String getContactName() {
-        return contactName;
-    }
-
-    public void setContactName(String contactName) {
-        this.contactName = contactName;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
+    /**
+     * toString needed for tableview mishaps
+     * @return this.contactName
+     */
     public String toString() {
         return this.contactName;
     }

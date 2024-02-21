d2ca572f998d1ff23ed1f9e71a5927a53691b1b9
JacobProut <Jprout@wgu.edu>
2024-02-21 00:10:08 -0600

    Finished JavaDoc Comments

--- src/model/Countries.java
+++ src/model/Countries.java
@@ -1,15 +1,19 @@
 package model;
 
-import DAO.CountriesDAO;
-
 import java.time.LocalDateTime;
 
+/**
+ * Countries class is created for recording objects into tables/ComboBoxes
+ */
 public class Countries {
-
-    //Primary Key
+    /**
+     * Primary Key Declaration
+     */
     private int countryId;
 
-
+    /**
+     * Declarations
+     */
     private String country;
     private LocalDateTime createDate;
     private String createdBy;
@@ -26,14 +30,36 @@ public class Countries {
         this.lastUpdatedBy = lastUpdatedBy;
     }
 
+    /**
+     * Constructor used in CountriesDAO.getAllCountriesList() & CountriesDAO.getSelectedCustomerCountry(int)
+     * @param countryId
+     * @param country
+     */
+    public Countries(int countryId, String country) {
+        this.countryId = countryId;
+        this.country = country;
+    }
+
+    /**
+     * getCountryId() gets countryId
+     * @return countryId
+     */
     public int getCountryId() {
         return countryId;
     }
 
+    /**
+     * setCountryId(int countryId) sets countryId
+     * @param countryId
+     */
     public void setCountryId(int countryId) {
         this.countryId = countryId;
     }
 
+    /**
+     * getCountry() gets country
+     * @return country
+     */
     public String getCountry() {
        return country;
     }
@@ -42,18 +68,35 @@ public class Countries {
         this.country = country;
     }
 
+    /**
+     * getCreateDate() gets createDate
+     * @return createDate
+     */
     public LocalDateTime getCreateDate() {
         return createDate;
     }
 
+    /**
+     * setCreateDate() sets createDate
+     * @param createDate
+     */
     public void setCreateDate(LocalDateTime createDate) {
         this.createDate = createDate;
     }
 
+    /**
+     * getCreatedBy() gets createdBy
+     * @return createdBy
+     */
     public String getCreatedBy() {
         return createdBy;
     }
 
+
+    /**
+     * setCreatedBy(String createdBy) sets createdBy
+     * @param createdBy
+     */
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
@@ -66,20 +109,26 @@ public class Countries {
         this.lastUpdate = lastUpdate;
     }
 
+    /**
+     * getLastUpdatedBy() get lastUpdatedBy
+     * @return lastUpdatedBy
+     */
     public String getLastUpdatedBy() {
         return lastUpdatedBy;
     }
 
+    /**
+     * setLastUpdatedBy(String lastUpdatedBy) sets lastUpdatedBy
+     * @param lastUpdatedBy
+     */
     public void setLastUpdatedBy(String lastUpdatedBy) {
         this.lastUpdatedBy = lastUpdatedBy;
     }
 
-
-    public Countries(int countryId, String country) {
-        this.countryId = countryId;
-        this.country = country;
-    }
-
+    /**
+     * toString() needed for tableview mishaps
+     * @return this.country
+     */
     public String toString() {
         return this.country;
     }
--- src/model/First_Level_Divisions.java
+++ src/model/First_Level_Divisions.java
@@ -2,22 +2,40 @@ package model;
 
 import java.time.LocalDateTime;
 
+/**
+ * First_Level_Divisions class is mostly created for ComboBoxes and filling in table data
+ */
 public class First_Level_Divisions {
 
-    //Primary Key
+    /**
+     * Primary Key Declaration
+     */
     private int divisionId;
 
-    //Foreign Key
+    /**
+     * Foreign key Declarations
+     */
     private int countryId;
 
-
+    /**
+     * Declarations
+     */
     private String divisionName;
     private LocalDateTime createDate;
     private String createdBy;
     private LocalDateTime lastUpdate;
     private String lastUpdatedBy;
 
-    //Not sure if I need countryId in here
+    /**
+     * Constructor for First_Level_DivisionsDAO.getAllDivisions()
+     * @param divisionId
+     * @param divisionName
+     * @param createDate
+     * @param createdBy
+     * @param lastUpdate
+     * @param lastUpdatedBy
+     * @param countryId
+     */
     public First_Level_Divisions(int divisionId, String divisionName, LocalDateTime createDate, String createdBy, LocalDateTime lastUpdate, String lastUpdatedBy, int countryId) {
         this.divisionId = divisionId;
         this.divisionName = divisionName;
@@ -25,80 +43,118 @@ public class First_Level_Divisions {
         this.createdBy = createdBy;
         this.lastUpdate = lastUpdate;
         this.lastUpdatedBy = lastUpdatedBy;
-        //not sure if countryId is supposed to be here
         this.countryId = countryId;
     }
 
+    /**
+     * Constructor for First_Level_DivisionDAO.countryToDivision(int)
+     * @param divisionId
+     * @param divisionName
+     * @param countryId
+     */
     public First_Level_Divisions(int divisionId, String divisionName, int countryId) {
         this.divisionId = divisionId;
         this.divisionName = divisionName;
         this.countryId = countryId;
     }
 
+    /**
+     * Constructor for First_Level_Divisions.getSelectedCustomerDivisionsLevel(int)
+     * @param selectedDivId
+     * @param selectedDivName
+     */
     public First_Level_Divisions(int selectedDivId, String selectedDivName) {
         this.divisionId = selectedDivId;
         this.divisionName = selectedDivName;
     }
 
-    //Do i need these get/set functions for the foreign key?
+    /**
+     * getCountryId() get countryId
+     * @return countryId
+     */
     public int getCountryId() {
         return countryId;
     }
+
+    /**
+     * setCountryId(Int countryId) set countryId
+     * @param countryId
+     */
     public void setCountryId(int countryId) {
         this.countryId = countryId;
     }
 
-
+    /**
+     * getDivisionId() gets divisionId
+     * @return divisionId
+     */
     public int getDivisionId() {
         return divisionId;
     }
 
-    public void setDivisionId(int divisionId) {
-        this.divisionId = divisionId;
-    }
-
-    public String getDivisionName() {
-        return divisionName;
-    }
-
-    public void setDivisionName(String divisionName) {
-        this.divisionName = divisionName;
-    }
-
+    /**
+     * getCreateDate() gets createDate
+     * @return createDate
+     */
     public LocalDateTime getCreateDate() {
         return createDate;
     }
 
+    /**
+     * setCreateDate(LocalDateTime createDate) sets createDate
+     * @param createDate
+     */
     public void setCreateDate(LocalDateTime createDate) {
         this.createDate = createDate;
     }
 
+    /**
+     * getCreatedBy() gets createdBy
+     * @return createdBy
+     */
     public String getCreatedBy() {
         return createdBy;
     }
 
+    /**
+     * setCreatedBy(String createdBy) sets createdBy
+     * @param createdBy
+     */
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
 
-    public LocalDateTime getLastUpdate() {
+    /**
+     * getLastUpdatedBy() gets lastUpdatedBy
+     * @return lastUpdatedBy
+     */
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    /**
+     * setLastUpdatedBy(String lastUpdatedBy)  sets lastUpdatedBy
+     * @param lastUpdatedBy
+     */
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    /**
+     * toString() needed for tableview mishaps
+     * @return this.divisionName
+     */
+    public String toString() {
+        return this.divisionName;
+    }
+
+    //MAYBE be used in a later program version
+    /*  public LocalDateTime getLastUpdate() {
         return lastUpdate;
     }
 
     public void setLastUpdate(LocalDateTime lastUpdate) {
         this.lastUpdate = lastUpdate;
-    }
-
-    public String getLastUpdatedBy() {
-        return lastUpdatedBy;
-    }
-
-    public void setLastUpdatedBy(String lastUpdatedBy) {
-        this.lastUpdatedBy = lastUpdatedBy;
-    }
-    public String toString() {
-        return this.divisionName;
-    }
-
+    }*/
 
 }
